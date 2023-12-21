1ce99cd29ae69af6dbaa62e837f5798872d7ff4e
Phoebe Scaccia <phoebescaccia@gmail.com>
2023-12-21 00:28:16 -0800

    Update column formatting

--- helpers/database.py
+++ helpers/database.py
@@ -20,13 +20,13 @@ class Database():
     def insert(query):
         if Database.connection == None:
             Database.connect()
-        
+
         cursor = Database.connection.cursor()
         cursor.execute(query)
         cursor.close()
         Database.connection.commit()
-        
+
     def connect():
         package_dir = os.path.abspath(os.path.dirname(__file__))
         database_path = os.path.join(package_dir, "..", "..", "phoebot.db")
-        Database.connection = sqlite3.connect(database_path)
+        Database.connection = sqlite3.connect(database_path, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)

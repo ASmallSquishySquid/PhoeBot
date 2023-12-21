9801ebc793751dc548a9550c80cd3e1d70ffaf2e
Phoebe Scaccia <2scacciaph2012+git@gmail.com>
2023-12-20 19:07:34 -0800

    Fix static variable

--- helpers/authorizedusers.py
+++ helpers/authorizedusers.py
@@ -2,31 +2,30 @@ import os
 import sqlite3
 
 class AuthorizedUsers():
-    def __init__(self):
-        self.users = set()
-        self.connection = None
+    users = set()
+    connection = None
 
-    def getUserSet(self):
-        if (len(self.users) == 0):
-            if (self.connection == None):
+    def getUserSet():
+        if (len(AuthorizedUsers.users) == 0):
+            if (AuthorizedUsers.connection == None):
                 package_dir = os.path.abspath(os.path.dirname(__file__))
                 database_path = os.path.join(package_dir, "..", "..", "phoebot.db")
-                self.connection = sqlite3.connect(database_path)
+                AuthorizedUsers.connection = sqlite3.connect(database_path)
            
-            cursor = self.connection.cursor()
+            cursor = AuthorizedUsers.connection.cursor()
             cursor.execute("""SELECT id FROM users;""")
             return set(cursor.fetchall())
         else:
-            return self.users
+            return AuthorizedUsers.users
     
-    def addUser(self, userId, username):
-        self.users.add(userId)
+    def addUser(userId, username):
+        AuthorizedUsers.users.add(userId)
         insert = """INSERT INTO users VALUES ({}, "{}");""".format(userId, username)
 
-        if (self.connection == None):
+        if (AuthorizedUsers.connection == None):
             package_dir = os.path.abspath(os.path.dirname(__file__))
             database_path = os.path.join(package_dir, "..", "..", "phoebot.db")
-            self.connection = sqlite3.connect(database_path)
-        cursor = self.connection.cursor()
+            AuthorizedUsers.connection = sqlite3.connect(database_path)
+        cursor = AuthorizedUsers.connection.cursor()
         cursor.execute(insert)
-        self.connection.commit()
+        AuthorizedUsers.connection.commit()

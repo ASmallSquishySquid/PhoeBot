24b25f64f03f4c079cca45b511bbd2d3aff79132
Phoebe Scaccia <phoebescaccia@gmail.com>
2023-12-22 11:56:29 -0800

    Save log

--- phoebot.py
+++ phoebot.py
@@ -1,15 +1,20 @@
+import sys
 import discord
 import os
+import datetime
 
 from discord.ext.commands import Bot
+
+sys.stdout = open("../phoebot-{}.log".format(datetime.datetime.now().strftime("%m:%d:%Y-%H:%M")), 'w')
+sys.stderr = sys.stdout
 
 cogs = ["textcommands", "loops", "events", "reminders"]
 
 class PhoeBot(Bot):
     def __init__(self, command_prefix, intents: discord.Intents, activity):
         super().__init__(command_prefix=command_prefix, intents=intents, activity=activity)
-    
-       
+
+
 bot = PhoeBot(command_prefix="!", intents=discord.Intents.all(), activity=discord.Activity(type=discord.ActivityType.watching, name="you 👀"))
 
 @bot.event

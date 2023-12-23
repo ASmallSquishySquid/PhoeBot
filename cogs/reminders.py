9fbada6a868e9f43cac76beffd02c47482a99659
Phoebe Scaccia <phoebescaccia@gmail.com>
2023-12-22 19:00:11 -0800

    Hide debug command

--- cogs/reminders.py
+++ cogs/reminders.py
@@ -97,7 +97,8 @@ class Reminders(commands.Cog):
             await ctx.send('Please keep the reminder in one string and keep all time components separate. And no AM/PM! <:charmanderawr:837344550804127774>')
 
     @commands.command(
-        help="Get the contents of the internal reminder list"
+        help="Get the contents of the internal reminder list",
+        hidden=True
     )
     async def debug(self, ctx: commands.Context):
         await ctx.send(self.reminderList)

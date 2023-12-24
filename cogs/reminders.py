0fa63abd671ea5f41e123c679c6b92d3b2d6dbbc
Phoebe Scaccia <phoebescaccia@gmail.com>
2023-12-23 20:13:45 -0800

    Save message to delete button

--- cogs/reminders.py
+++ cogs/reminders.py
@@ -87,7 +87,8 @@ class Reminders(commands.Cog):
 
         embedMessage = discord.Embed(title="Reminder created <:charmanderawr:837344550804127774>", description=reminder, color=discord.Color.og_blurple())
         embedMessage.add_field(name="Scheduled Time", value=dateParam.strftime("%m/%d/%Y at %H:%M"))
-        await ctx.send(embed=embedMessage, view=DeleteButton(self, embedMessage, id))
+        buttons = DeleteButton(self, embedMessage, id)
+        buttons.message = await ctx.send(embed=embedMessage, view=buttons)
 
     @remind.error
     async def remind_error(self, ctx, error):

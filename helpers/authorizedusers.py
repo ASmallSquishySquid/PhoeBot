ef1322964544c41582feac0fb8a7a445cabd0af5
Phoebe Scaccia <phoebescaccia@gmail.com>
2023-12-27 21:32:21 -0800

    Add docstrings to helper functions

--- helpers/authorizedusers.py
+++ helpers/authorizedusers.py
@@ -5,14 +5,26 @@ class AuthorizedUsers():
     UNAUTHORIZED_MESSAGE = "Sorry, I don't talk to strangers <:mmSweatUhhMocha:764772302403272704>"
 
     def startup():
+        """Builds the authorized user cache"""
         userIds = Database.select("id", "users")
         for userId in userIds:
             AuthorizedUsers.users.add(userId[0])
 
     def get_user_set() -> set:
+        """Get the authorized users in the cache
+
+        Returns:
+            set: The set of user IDs
+        """
         return AuthorizedUsers.users
 
     def add_user(userId: int, username: str) -> None:
+        """Add a user to the authorized users list
+
+        Args:
+            userId (int): The user ID
+            username (str): A recognizable username
+        """
         if userId in AuthorizedUsers.users:
             return
 
@@ -20,6 +32,11 @@ class AuthorizedUsers():
         Database.insert("users", f"""{userId}, "{username}" """, True)
     
     def remove_user(userId: int):
+        """Removes a user from the authorized users list
+
+        Args:
+            userId (int): the user ID
+        """
         if not userId in AuthorizedUsers.users:
             return
         
@@ -27,4 +44,12 @@ class AuthorizedUsers():
         Database.delete("users", f"WHERE id={userId}")
 
     def is_authorized(userId: int) -> bool:
+        """Checks if a user is authorized
+
+        Args:
+            userId (int): The user ID
+
+        Returns:
+            bool: `True` if the user is authorized, `False` otherwise
+        """
         return userId in AuthorizedUsers.users
--- helpers/database.py
+++ helpers/database.py
@@ -5,6 +5,16 @@ class Database():
     connection = None
 
     def select(columns: str, table: str, filters: str = "") -> list:
+        """Performs a SELECT query in the phoebot database.
+
+        Args:
+            columns (str): The columns to select from
+            table (str): The table to use
+            filters (str, optional): Any filters to apply to the query. Defaults to "".
+
+        Returns:
+            list: The results
+        """
         if Database.connection is None:
             Database.connect()
 
@@ -18,6 +28,13 @@ class Database():
         return results
 
     def insert(table_and_cols: str, values: str, ignore: bool = False) -> None:
+        """Performs an INSERT query in the phoebot database.
+
+        Args:
+            table_and_cols (str): The tables and columns being used
+            values (str): The values to insert
+            ignore (bool, optional): Flag to ignore duplicates. Defaults to False.
+        """
         if Database.connection is None:
             Database.connect()
 
@@ -28,6 +45,12 @@ class Database():
         Database.connection.commit()
 
     def delete(table: str, filters: str = "") -> None:
+        """Performs a DELETE query in the phoebot database.
+
+        Args:
+            table (str): The table to use
+            filters (str, optional): The filters to use. Defaults to "".
+        """
         if Database.connection is None:
             Database.connect()
 
@@ -37,6 +60,15 @@ class Database():
         Database.connection.commit()
 
     def count(table: str, filters: str = "") -> int:
+        """Performs a COUNT query in the phoebot database.
+
+        Args:
+            table (str): The table to use
+            filters (str, optional): The filters to use. Defaults to "".
+
+        Returns:
+            int: The resulting count
+        """
         if Database.connection is None:
             Database.connect()
 
@@ -50,6 +82,14 @@ class Database():
         return count
     
     def query(query: str) -> list:
+        """Performs a query in the phoebot database.
+
+        Args:
+            query (str): The query string
+
+        Returns:
+            list: The results
+        """
         if Database.connection is None:
             Database.connect()
 
@@ -62,6 +102,7 @@ class Database():
         return result
 
     def connect() -> None:
+        """Connect to the phoebot database"""
         package_dir = os.path.abspath(os.path.dirname(__file__))
         database_path = os.path.join(package_dir, "..", "..", "phoebot.db")
         Database.connection = sqlite3.connect(database_path, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
